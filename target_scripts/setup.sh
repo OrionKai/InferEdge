b49c8dbe99f441c4938b12e833e75c1b4171e608
Gabriel Orion Kai Antar <goka1@st-andrews.ac.uk>
2025-03-26 17:50:49 +0000

    Add setup code to install Docker, Python

--- target_scripts/setup.sh
+++ target_scripts/setup.sh
@@ -13,7 +13,6 @@ function main() {
     arch=$2
 
     suite_path="/home/$USERNAME/$suite_name"
-    suite_directory="$suite_path/$suite_name"
 
     sudo apt-get update
 
@@ -24,11 +23,13 @@ function main() {
         enable_memory_controller
     fi
 
+    setup_docker
     load_docker_image
     setup_cadvisor
     setup_prometheus
     setup_python
 
+    # TODO: test if Darwin actually shows up on Ubuntu VM on Mac, else ask interactively
     if [ "$uname -m" != "Darwin" ]; then
         aot_compile_wasm_non_mac
     else
@@ -59,33 +60,64 @@ function enable_memory_controller() {
     fi
 }
 
+function setup_docker() {
+    if ! command -v docker &> /dev/null; then
+        echo "Docker is not installed. Would you like to install Docker through the script?"
+            echo "1. Yes"
+            echo "2. No"
+        read -p "Enter the number identifying your choice: " choice
+        if [ "$choice" == "1" ]; then
+            chmod u+x "$suite_path"/docker/install-docker.sh
+            sudo "$suite_path"/docker/install-docker.sh
+        else
+            echo "Please install Docker manually and re-run this script."
+            exit 1
+        fi
+    fi
+}
+
 function load_docker_image() {
     docker load -i "image-classification-$arch.tar"
 }
 
 function setup_cadvisor() {
-    chmod u+x $suite_directory/cadvisor/cadvisor
-    sudo apt-get install libpfm4 linux-perf
+    chmod u+x $suite_path/cadvisor/cadvisor
+    sudo apt install libpfm4 linux-perf
 }
 
 function setup_prometheus() {
-    chmod u+x $suite_directory/prometheus/prometheus
+    chmod u+x $suite_path/prometheus/prometheus
 }
 
 function setup_python() {
-    sudo apt install -y python3 python3-venv python3-pip cgroup-tools
-    python3 -m venv "$suite_directory/python/myenv" 
-    source "$suite_directory/python/myenv/bin/activate" && pip install -r requirements.txt
+    # Check if Python is installed, if not, install it
+    if ! command -v python3 &> /dev/null
+    then
+        echo "Python3 is not installed. Would you like to install Python3 through the script?"
+            echo "1. Yes"
+            echo "2. No"
+        read -p "Enter the number identifying your choice: " choice
+        if [ "$choice" == "1" ]; then
+            sudo apt install -y python3 python3-venv python3-pip
+        else
+            echo "Please install Python3 manually and re-run this script."
+            exit 1
+        fi
+    fi
+
+    sudo apt install -y cgroup-tools
+    python3 -m venv "$suite_path/myenv" 
+    source "$suite_path/myenv/bin/activate" && pip install -r "$suite_path/python/target/requirements.txt"
 }
 
 function aot_compile_wasm_non_mac() {
     # AoT compile the WebAssembly code (if not on Mac)
-    wasmedge compile "$suite_directory/wasm/interpreted.wasm" "$suite_directory/wasm/aot.wasm"
+    wasmedge compile "$suite_path/wasm/interpreted.wasm" "$suite_path/wasm/aot.wasm"
 }
 
 function aot_compile_wasm_mac() {
     # AoT compile the WebAssembly code (if on Mac)
-    wasmedge compile "$suite_directory/wasm/interpreted.wasm" "$suite_directory/wasm/aot.so"
+    wasmedge compile "$suite_path/wasm/interpreted.wasm" "$suite_path/wasm/aot.so"
 }
 
 main
